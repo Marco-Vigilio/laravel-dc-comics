20665a2d8370a7f4abe968128a3d5cf1a7936b2a
Marco-Vigilio <tonyvigilio@gmail.com>
2023-07-26 17:05:53 +0200

    faccio una tabella per la visualizzazione dei miei comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,17 +3,59 @@
 @section('title', 'Welcome')
 
 @section('main-content')
-    <h1>
+<div class="contain">
+<h1>
         Comics
     </h1>
-    <div>
-        @foreach($fumetto as $comic)
-            <li>
-                <a href=  "{{ route('comics.show', $comic->id) }}" >
-                    {{$comic->title}} - {{$comic->price}}
-                </a>
-                
-            </li>
-        @endforeach
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-striped table-hover text-center table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Serie</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($fumetto as $comic)
+                        <tr>
+                            <th scope="row">
+                                {{ $comic->id }}
+                            </th>
+                            <td>
+                                {{ $comic->title  }}
+                            </td>
+                            <td>
+                                {{ $comic->series  }}
+                            </td>
+                            <td>
+                                <a class="btn btn-sm btn-primary me-2"
+                                    href="{{ route('comics.show',$comic->id)}}">
+                                    View
+                                </a>
+                                <a class="btn btn-sm btn-success me-2"
+                                    href="#">
+                                    Edit
+                                </a>
+                                <form action="#" class="d-inline form-terminator" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button type="submit" class="btn btn-sm btn-warning me-2">
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
+
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
+</div>
+    
+    
 @endsection
